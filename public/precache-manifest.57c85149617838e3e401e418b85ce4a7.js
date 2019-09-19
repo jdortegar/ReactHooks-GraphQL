8c27f4cc2a7de0b2e105a535f470cc93befff351
David Ortega <david.ortega@habla.io>
2019-09-19 13:11:13 -0300

    Finish

---
+++ public/precache-manifest.57c85149617838e3e401e418b85ce4a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62174612b956ba96be41423b7ddd129a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e4e9e86be569d117707",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "19c1e84267a9daf8ba50",
+    "url": "/static/js/2.4ff62e1e.chunk.js"
+  },
+  {
+    "revision": "3e4e9e86be569d117707",
+    "url": "/static/js/main.64d8750c.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
